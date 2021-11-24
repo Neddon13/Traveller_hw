138352b185398b2cf186d8443fda32733acb3ad5
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-11-24 16:44:41 +0000

    10 passing

---
+++ models/traveller.js
@@ -0,0 +1,30 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journeys => journeys.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journeys => journeys.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journeys => transport === journeys.transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journeys => journeys.distance > minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((acc, currentJourney) => acc + currentJourney.distance, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
