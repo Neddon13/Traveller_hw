b32ded4b98a33798de340d5711b17aacf72382fa
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-11-25 09:31:07 +0000

    11 passing

--- models/traveller.js
+++ models/traveller.js
@@ -23,8 +23,15 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+    return this.journeys.map((journey) => {
+      return journey.transport;
+    })
+    .filter((transport, index, array) => {
+      return array.indexOf(transport) === index;
+    });
+  };
 
-};
+
 
 
 module.exports = Traveller;
